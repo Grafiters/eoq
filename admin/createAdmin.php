337336ee414e5ab0b02aebcaf0cea0b43fa3e44b
saifoelloh <saifoelloh@gmail.com>
2020-04-04 19:24:40 +0700

    minor

--- admin/createAdmin.php
+++ admin/createAdmin.php
@@ -5,17 +5,21 @@ if (isset($_POST['submit'])) {
   $username=$_POST['username'];
   $name=$_POST['name'];
   $email=$_POST['email'];
-  $password=$_POST['password'];
+  $password=hash('sha512', $_POST['password']);
   $status=$_POST['status'];
 
   $result = mysqli_query($conn, "INSERT INTO user(username,name,email,password,status)VALUES('$username','$name','$email','$password','$status')");
 
   if($result){
-    header('location: /pages/admin');
-    die();
+    $message = 'Sukses membuat admin';
+    $status = true;
   }else{
-    echo "Error: " . $result . "<br>" . $conn->error;
+    $message = 'Gagal membuat admin';
+    $status = false;
   }
+
+  header("location: /pages/admin?msg=$message&status=$status");
+  die();
 }
 
 ?>
