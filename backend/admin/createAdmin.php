8054580ab4b8dd5b4e48afef876c324bd81ad2cd
Grafiters <bayugrafit@gmail.com>
2020-04-05 14:08:27 +0700

    little update

--- backend/admin/createAdmin.php
+++ backend/admin/createAdmin.php
@@ -4,19 +4,19 @@ include("../../Connect.php");
 if (isset($_POST['submit'])) {
   $username=$_POST['username'];
   $name=$_POST['name'];
-  $telp=$_POST['tlep'];
+  $telp=$_POST['phone'];
   $email=$_POST['email'];
   $password=$_POST['password'];
-  $status=$_POST['status'];
+  $status=$_POST['role'];
   
   $hashpas = hash('sha512',$password);
 
-  $inrole = substr($status,0,2);
-  $inname = substr($username,0,2);
+  $inrole = substr(strtoupper($status),0,2);
+  $inname = substr(strtoupper($username),0,2);
   $code = $inrole.$inname;
 
   $result = mysqli_query($conn, "INSERT INTO user(username,name,email,phone,password,role,code)VALUES('$username','$name','$email','$telp','$hashpas','$status','$code')");
-
+  
   if($result){
     header('location:  ../../pages/admin');
     die();
