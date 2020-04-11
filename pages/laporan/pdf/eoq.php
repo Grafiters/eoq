29d53d32ca3e4b62b6791cc4507d6a3a36e69325
saifoelloh <saifoelloh@gmail.com>
2020-04-11 16:27:34 +0700

    creating pdf template

---
+++ pages/laporan/pdf/eoq.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laporan Perhitugan EOQ</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Laporan Perhitugan EOQ</h1>
+        <table class="table table-bordered text-center">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Tanggal Perhitugan EOQ</th>
+                    <th>Nama Barang</th>
+                    <th>Kebutuhan Tahunan</th>
+                    <th>Biaya Sekali Pakai</th>
+                    <th>Biaya Simpan Barang</th>
+                    <th>Hasil EOQ</th>
+                    <th>Hasil Biaya Pesan</th>
+                    <th>Hasil Biaya Simpan</th>
+                    <th>ROP</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Senin, 20/04/2020</td>
+                    <td>Bio 7</td>
+                    <td>27.540</td>
+                    <td>Rp 130.000</td>
+                    <td>Rp 150</td>
+                    <td>6,909</td>
+                    <td>Rp 518.193</td>
+                    <td>Rp 518.175</td>
+                    <td>275</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
---
+++ pages/laporan/pdf/penjualan.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laporan Penjualan</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Laporan Penjualan</h1>
+        <table class="table table-bordered text-center">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Kode Penjualan</th>
+                    <th>Tanggal Pembelian</th>
+                    <th>Total Bayar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>KJ001</td>
+                    <td>Senin, 20/04/2020</td>
+                    <td>Rp 2.700.000</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
