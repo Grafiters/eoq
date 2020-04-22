3482ce8761c8499992dbe8db7f3bd13e4625695a
Grafiters <bayugrafit@gmail.com>
2020-04-23 01:31:10 +0700

    fix permission access

--- pages/item/create.php
+++ pages/item/create.php
@@ -1,5 +1,12 @@
 <?php
   include ('../../Connect.php');
+  session_start();
+  
+  if(!$_SESSION['role']=="admin"){
+    $messages = "Anda Tidak Mempunyai Access Untuk Melakukan Aksi Ini";
+    header("Location: /eoq/pages/item/index.php?msg=$messages");
+  }
+
   $query = $conn->query('SELECT MAX(id) as maxId FROM barang');
   // var_dump($query);
   $hasil = $query->fetch_assoc();
@@ -64,7 +71,7 @@
           <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block"><?php echo $_SESSION['username']; ?></a>
         </div>
       </div>
 
