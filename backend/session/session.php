d07de6a9dc84b6f0cc837e02be54d928bd3301ca
saifoelloh <saifoelloh@gmail.com>
2020-04-28 21:12:08 +0700

    refactoring

--- backend/session/session.php
+++ backend/session/session.php
@@ -23,14 +23,14 @@
          $_SESSION['username'] = $user_check;
          $_SESSION['role'] = "pengadaan";
          $role = "pengadaan";
-         header("Location:/eoq/pages/admin/index.php");
+         header("Location: /eoq/pages/admin/index.php");
          //    header("Location: ../index2.php", true);
          //    die();
       }else if($result['role'] == "penjualan"){
          $_SESSION['username'] = $user_check;
          $_SESSION['role'] = "penjualan";
          $role = "penjualan";
-         header("Location:/eoq/pages/penjualan/index.php");
+         header("Location: /eoq/pages/penjualan/index.php");
          //    header("Location: ../index2.php", true);
          //    die();
       }else{
