6a3d699119c61a03da92447104266735444cc14b
saifoelloh <saifoelloh@gmail.com>
2020-05-10 11:04:44 +0700

    change layout

--- pages/laporan/pdf/pembelian.php
+++ pages/laporan/pdf/pembelian.php
@@ -6,36 +6,73 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container">
-        <h1>Laporan Pembelian</h1>
-        <table class="table table-bordered text-center">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Kode Pembelian</th>
-                    <th>Tanggal Pembelian</th>
-                    <th>Total Bayar</th>
-                </tr>
-            </thead>
-            <tbody>
-              <?php
-                  $i = 1;
-                  while ($pembelian = $buys->fetch_array()) {
-                    $status = $i;
-                    $kode = $pembelian['kode'];
-                    $tanggal = date_format(date_create($pembelian['tanggal']), "D, d/m/Y");
-                    $totalHarga = number_format($pembelian['total'], 0);
-                    echo "<tr>";
-                      echo "<td>$i</td>";
-                      echo "<td>$kode</td>";
-                      echo "<td>$tanggal</td>";
-                      echo "<td>Rp $totalHarga</td>";
-                    echo "</tr>";
-                    $i++;
-                  }
-              ?>
-            </tbody>
-        </table>
-    </div>
+  <div class="text-center">
+    <h5>GTT GROUP</h5>
+    <br />
+    <h5>
+      Jl Mugas Dalem No 7 Semarang 50249<br>
+      Phone +622440124545<br>
+      Email Address: bio_7jateng@yahoo.com
+    </h5>
+    <hr width="100%">
+  </div>
+  <div>
+    <br />
+    <h5 class="text-center">Data Pembelian</h5>
+    <br />
+    <table class="table-borderless">
+      <tr>
+        <td>Tanggal Pembelian</td>
+        <td>
+          <?= ": ".date_format(date_create($temp['tanggal']), "d F Y") ?>
+        </td>
+      </tr>
+      <tr>
+        <td>Kode Pembelian</td>
+        <td>
+          <?= ": ".ucwords($temp['kode']) ?>
+        </td>
+      </tr>
+      <tr>
+        <td>Nama Supplier</td>
+        <td>
+          <?= ": ".ucwords($temp['pembeli']) ?>
+        </td>
+      </tr>
+    </table>
+    <br />
+    <table class="d-print table table-bordered text-center">
+      <thead>
+        <tr>
+          <th>Nama Barang</th>
+          <th>Harga Satuan</th>
+          <th>Jumlah Beli</th>
+          <th>Bayar</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+          $idx = 1;
+          while ($buy = $buys->fetch_assoc()) {
+            echo "<tr>";
+              echo "<td>$idx</td>";
+              echo "<td>".ucwords($buy['kode'])."</td>";
+              echo "<td>".date_format(date_create($buy['tanggal']), "l, d-m-Y")."</td>";
+              echo "<td>Rp ".number_format($buy['total'], 0)."</td>";
+            echo "</tr>";
+            $idx++;
+          }
+      ?>
+      </tbody>
+    </table>
+    <p class="text-right">
+      <b>Total Bayar: </b> <?= "Rp ".number_format(1000, 0) ?>
+    </p>
+    <br/>
+    <p>Admin/Bagian Pengadaan</p>
+    <br/>
+    <br/>
+    <p>TTD dan Nama Terang</p>
+  </div>
 </body>
 </html>
