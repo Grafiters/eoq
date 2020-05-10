70b6fb12cde293a53afa743c9e0d8bcc5e55619b
Grafiters <bayugrafit@gmail.com>
2020-05-10 15:37:56 +0700

    little update

---
+++ backend/pembelian/downloadtgl.php
@@ -0,0 +1,37 @@
+<?php
+include ('../../Connect.php');
+require '../../vendor/autoload.php';
+
+if ($conn) {
+  $query = "
+  SELECT
+        pembelian.id AS id,
+        pembelian.code AS kode,
+        supplier.name AS supplier,
+        pembelian.total AS total,
+        pembelian.created_at AS tanggal
+      FROM pembelian
+      INNER JOIN supplier
+      ON pembelian.supplier_id=supplier.id
+      WHERE pembelian.created_at BETWEEN '$awal' AND '$akhir'";
+  $buys = $conn->query($query);
+}
+// var_dump($buys);
+ob_start();
+?>
+
+<?= include('../../pages/laporan/pdf/pembelian.php') ?>
+
+<?php
+
+$html = ob_get_clean();
+$title = "Laporan Pembelian - ".date("d-m-Y");
+
+use Dompdf\Dompdf;
+
+$document = new Dompdf();
+$document->loadHtml($html);
+$document->setPaper('A4', 'portrait');
+$document->render();
+$document->stream($title,array("Attachment"=>0));
+?>
